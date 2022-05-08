526a0c22baf38f8066a9d82fa7fbcd3fceffa7cc
JasonHeoJung <S10222947@connect.np.edu.sg>
2022-05-08 19:42:30 +0800

    Files Added

---
+++ app/src/main/java/sg/edu/np/mad/myapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package sg.edu.np.mad.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.view.View;
+import android.widget.Button;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button follow = findViewById(R.id.follow);
+        user user1 = new user();
+        user1.followed = false;
+
+        follow.setOnClickListener(new View.OnClickListener(){
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view){
+                if (!user1.followed){
+                    follow.setText("follow");
+                    user1.followed = true;
+                }
+                else {
+                    follow.setText("unfollow");
+                    user1.followed = false;
+                }
+            }
+
+        });
+    }
+}
