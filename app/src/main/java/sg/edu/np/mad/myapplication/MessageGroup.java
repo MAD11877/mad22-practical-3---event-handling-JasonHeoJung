6565c1b36c110c83ec57973014b8e53feaa419ab
JasonHeoJung <S10222947@connect.np.edu.sg>
2022-08-02 13:46:10 +0800

    Challenge Completed

--- app/src/main/java/sg/edu/np/mad/myapplication/MessageGroup.java
+++ app/src/main/java/sg/edu/np/mad/myapplication/MessageGroup.java
@@ -9,14 +9,13 @@ import android.view.View;
 import android.widget.Button;
 
 public class MessageGroup extends AppCompatActivity {
-
-    Button Group1 = findViewById(R.id.group1);
-    Button Group2 = findViewById(R.id.group2);
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_group);
+
+        Button Group1 = findViewById(R.id.group1);
+        Button Group2 = findViewById(R.id.group2);
 
         Group1.setOnClickListener(new View.OnClickListener() {
             @Override
